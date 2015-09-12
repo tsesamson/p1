0b82c9268dc18cd4f7aebce0d004638182df6f28
Samson Tse <tse.samson@gmail.com>
2015-09-12 12:59:40 -0400

    Uploading initial README file and index.php with profile template

---
+++ README.md
@@ -0,0 +1,17 @@
+# Samson Tse's Project 1
+
+## Live URL
+<http://p1.samsontse.com>
+
+## Description
+The purpose of this project is to create a nice profile page and adapting the Git deployment workflow.
+
+## Demo/Screencast
+Not required for this project.
+
+## Details for teaching team
+No login required for this profile page.
+
+## Outside code
+* Bootstrap: http://getbootstrap.com
+* Bootstrap Theme: http://bootswatch.com/flatly/
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Samson Tse P1 - DWA15 Portfolio</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+	
+	<style>
+		h1 {
+			text-align:center;
+		}
+	
+		.container {
+			width:50%;
+			min-width:600px;
+			margin-bottom:50px;
+		}	
+		
+		img {
+			display:block;
+			margin:auto;
+			width:50%;
+		}
+	
+	</style>
+
+</head>
+<body>
+
+	<div class="container">
+	
+		<h1>Samson Tse <br> Dynamic Web Apps Portfolio</h1>
+		
+		<img src='https://s3.amazonaws.com/perfectshutter/website/images/profile/ps/ps-profile-samson.jpg' alt="Samson Tse's Profile Photo">
+		
+		<h3>Project 2 Title</h3>
+		View: [link]
+		Github: [link]
+		
+		<h3>Project 3 Title</h3>
+		View: [link]
+		Github: [link]
+		
+		<h3>Project 4 Title</h3>
+		View: [link]
+		Github: [link]
+
+		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		
+		<h2>Why are you taking this course?</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		
+		<h2>What previous experience do you have with programming?</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		
+		<h2>Anything else we should know?</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	
+	</div>
+	
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	
+</body>
+</html>
