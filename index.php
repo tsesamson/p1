76dbb85a5dbe1782f445caf7796f4ae7efecb197
Samson Tse <tse.samson@gmail.com>
2015-09-12 13:21:15 -0400

    Changed all links for live and github url on main page

--- index.php
+++ index.php
@@ -37,17 +37,38 @@
 		
 		<img src='https://s3.amazonaws.com/perfectshutter/website/images/profile/ps/ps-profile-samson.jpg' alt="Samson Tse's Profile Photo">
 		
-		<h3>Project 2 Title</h3>
-		View: [link]
-		Github: [link]
+		<h3>Project 2 Title Placeholder</h3>
+		<div class="input-group" style="font-family: Arial;">
+      <span class="input-group-addon" style="font-family: Arial;">Live</span>
+      <input type="text" class="form-control" style="font-family: Arial;" value="http://p2.samsontse.com">
+    </div>
+    <br/>
+		<div class="input-group" style="font-family: Arial;">
+      <span class="input-group-addon" style="font-family: Arial;">Github</span>
+      <input type="text" class="form-control" style="font-family: Arial;" value="https://github.com/tsesamson/p2">
+    </div>
 		
-		<h3>Project 3 Title</h3>
-		View: [link]
-		Github: [link]
+		<h3>Project 3 Title Placeholder</h3>
+		<div class="input-group" style="font-family: Arial;">
+      <span class="input-group-addon" style="font-family: Arial;">Live</span>
+      <input type="text" class="form-control" style="font-family: Arial;" value="http://p3.samsontse.com">
+    </div>
+    <br/>
+		<div class="input-group" style="font-family: Arial;">
+      <span class="input-group-addon" style="font-family: Arial;">Github</span>
+      <input type="text" class="form-control" style="font-family: Arial;" value="https://github.com/tsesamson/p3">
+    </div>
 		
-		<h3>Project 4 Title</h3>
-		View: [link]
-		Github: [link]
+		<h3>Project 4 Title Placeholder</h3>
+		<div class="input-group" style="font-family: Arial;">
+      <span class="input-group-addon" style="font-family: Arial;">Live</span>
+      <input type="text" class="form-control" style="font-family: Arial;" value="http://p4.samsontse.com">
+    </div>
+    <br/>
+		<div class="input-group" style="font-family: Arial;">
+      <span class="input-group-addon" style="font-family: Arial;">Github</span>
+      <input type="text" class="form-control" style="font-family: Arial;" value="https://github.com/tsesamson/p4">
+    </div>
 
 		<h2>In 2+ sentences describe who you are and what you do.</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
